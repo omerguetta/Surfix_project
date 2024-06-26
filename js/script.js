78620aea3ddaec3671fd4a8237eaf21e9d6c2589
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-06-26 15:12:00 +0300

    add navbar bg-body-tertiary
    add the part that the user can go back and add to favorite the beach
    also fix the css

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ const view_all = document.querySelector('.view_all_btn');
 function getBeaches(){
     
 }
+document.addEventListener('DOMContentLoaded', function () {
+    let backButton = document.getElementById('backButton');
+    let plusButton = document.getElementById('plusButton');
+
+    backButton.addEventListener('click', function () {
+        this.classList.add('btn-disabled');
+    });
+
+    plusButton.addEventListener('click', function () {
+        this.classList.add('btn-disabled');
+    });
+});
