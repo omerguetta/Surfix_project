b94cdfbc80759a7d65c38cd18c60802269b0277c
Arielle Farhi <ariel.farhi@stu.shenkar.ac.il>
2024-07-24 20:38:00 +0300

    Add sign_up page

---
+++ client/js/logIn.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('sign-up-btn').addEventListener('click', function() {
+        window.location.href = 'sign_up.html'; 
+    });
+});
