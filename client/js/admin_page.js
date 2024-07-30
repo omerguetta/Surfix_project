1e839b07c377d233d04b0fed9dc1e054d577b3df
omersegev2496 <omer.segev@stu.shenkar.ac.il>
2024-07-31 01:06:17 +0300

    fixing bugs again

--- client/js/admin_page.js
+++ client/js/admin_page.js
@@ -6,7 +6,7 @@ function createUserItem(UserData) {
 
     const account = document.createElement('img');
     account.className = 'account-img';
-    account.src = '../images/acount-icon.png';
+    account.src = './images/acount-icon.png';
     account.alt = 'account';
 
     const title = document.createElement('p');
@@ -17,12 +17,12 @@ function createUserItem(UserData) {
 
     const deleteU = document.createElement('img');
     deleteU.className = 'delete_img';
-    deleteU.src = '../images/remove-icon.png';
+    deleteU.src = './images/remove-icon.png';
     deleteU.alt = 'delete';
 
     const addU = document.createElement('img');
     addU.className = 'add_img';
-    addU.src = '../images/person-icon.png';
+    addU.src = './images/person-icon.png';
     addU.alt = 'add';
 
     const userDetails = document.createElement('div');
@@ -81,7 +81,7 @@ window.onload = async () => {
     // document.getElementById('sortByName').addEventListener('change', updateFilters);
     // document.getElementById('searchInput').addEventListener('input', updateFilters);
     // document.querySelector('.add-new-beach').addEventListener('click', ()=>{
-    //     window.location.href = '../pages/beach_form.html';
+    //     window.location.href = './pages/beach_form.html';
     // });
     // document.getElementById('maxDistance').addEventListener('change', async (event) => {
     //     document.getElementById('rangeValue').textContent = event.target.value;
